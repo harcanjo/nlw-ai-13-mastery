c277605819beb902b1b687615282b6d6e1f4ccda
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-09-12 22:46:01 -0300

    header main aside and footer definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Separator } from "@radix-ui/react-separator";
 
 export function App() {
   return (
-    <Button>Teste</Button>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            GitHub
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+
+          </div>
+
+          <p className="text-sm text-muted-foreground">Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.</p>
+        </div>
+        <aside className="w-80">
+        </aside>
+      </main>
+    </div>
   )
 }
