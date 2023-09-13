9b5156fc427fa816cc22330cd8f8dc6ca12a983a
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-09-12 23:40:51 -0300

    aside form prompt select

--- src/App.tsx
+++ src/App.tsx
@@ -74,8 +74,20 @@ export function App() {
 
           <form className="space-y-6">
             <div className="space-y-2 ">
-              <Label>Modelo</Label>
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">Título do YouTube</SelectItem>
+                  <SelectItem value="description">Descrição do YouTube</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
 
+            <div className="space-y-2 ">
+              <Label>Modelo</Label>
               <Select disabled defaultValue="gpt3.5">
                 <SelectTrigger>
                   <SelectValue />
