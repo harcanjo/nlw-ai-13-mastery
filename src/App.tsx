969774523389d3314ad00e2ee0a6db295139ec56
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-09-12 23:37:29 -0300

    aside form submit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Github, FileVideo, Upload } from "lucide-react";
+import { Github, FileVideo, Upload, Wand2 } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Separator } from "@radix-ui/react-separator";
 import { Textarea } from "./components/ui/textarea";
@@ -103,6 +103,13 @@ export function App() {
                 Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
               </span>
             </div>
+
+            <Separator />
+
+            <Button type="submit" className="w-full">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
 
           </form>
         </aside>
