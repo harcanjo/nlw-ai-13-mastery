37c778599fe6a5d47b28507397a3bf492225aacf
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-09-14 12:00:39 -0300

    textarea for result working

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -18,12 +18,18 @@ export function App() {
     input,
     setInput,
     handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
   } = useCompletion({
     api: 'http://localhost:3333/ai/complete',
     body: {
       videoId,
       temperature,
     },
+    headers: {
+      'Content-type': 'application/json',
+    }
   })
 
   return (
@@ -58,6 +64,7 @@ export function App() {
               className="resize-none p-4 leading-relaxed"
               placeholder="Resultado gerado pela IA..."
               readOnly
+              value={completion}
             />
           </div>
 
@@ -68,7 +75,7 @@ export function App() {
 
           <Separator />
 
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt: </Label>
               <PromptSelect onPromptSelected={setInput} />
@@ -107,7 +114,7 @@ export function App() {
 
             <Separator />
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isLoading} type="submit" className="w-full">
               Executar
               <Wand2 className="w-4 h-4 ml-2" />
             </Button>
